71b3a767f4d2c53ae08b9def2b35b683144fb668
Princerwtt <109853314+Princerwtt@users.noreply.github.com>
2022-10-09 16:31:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# scoping-project
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-s62h6t)
---
+++ script.js
@@ -0,0 +1,14 @@
+function test() {
+  console.log('prince');
+}
+test();
+
+function test1(name) {
+  console.log(name);
+  test2(5, 5, 5);
+}
+test1('digikull');
+
+function test2(a, b, c) {
+  console.log(a + b + c);
+}
